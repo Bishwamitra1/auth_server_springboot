57f2d6e21f3a4b5e83d0a1112cf7d691a7bd1cf5
Bishwamitra1 <sapkotabishwamitra@gmail.com>
2023-02-25 18:42:04 -0600

    created spring security auth server and made changes to pom.xml

---
+++ src/main/java/com/example/auth_server_springboot/AuthServerSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.auth_server_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServerSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServerSpringbootApplication.class, args);
+    }
+
+}
